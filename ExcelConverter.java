2e96bf41e2d3099d7545310c51b4152160cc4be1
MEdwards3222 <39884776+MEdwards3222@users.noreply.github.com>
2018-06-08 12:21:54 -0400

    Add files via upload

--- ExcelConverter.java
+++ ExcelConverter.java
@@ -9,6 +9,8 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 
 
 
@@ -16,13 +18,15 @@ import org.apache.poi.ss.usermodel.Cell;
 
 public class ExcelConverter {
 	
-	static File dir = new File("/Users/michaeledwards/Documents/ExcelConverterProject/Indiaanalyzed2018_05");
+	static File dir = new File("/Users/michaeledwards/Documents/ExcelConverterProject/Indiaanalyzed2018_05"); //Sets the filepath for one of the folders used to store .txt files. End program will include all three numbers.
 	static List<String> fileList = new ArrayList<String>();
+	
 	static String[] param;
-	static XSSFWorkbook workbook = new XSSFWorkbook();
-    static XSSFSheet firstSheet = workbook.createSheet("FIRST SHEET");
-    static XSSFRow row1 = firstSheet.createRow(0);
-    static String [] state = new String [] {
+	static XSSFWorkbook workbook = new XSSFWorkbook(); //Creates a new workbook (.xlsx file)
+    static XSSFSheet firstSheet = workbook.createSheet("FIRST SHEET"); //Creates a new sheet for the spreadsheet
+    static XSSFRow row1 = firstSheet.createRow(0); //"Creates" a row on the first sheet
+    static XSSFRow rowN = null; //For use in subsequent rows that isn't the first row that lists the parameters
+    static String [] state = new String [] { //First array that stores the states as outlined by the client.
     	"ATEX", "ATI", "C", "EMO+", "EMO-" ,
     	"EV+", "EV-", "G", "IP", "L",
     	"META", "O", "OE", "P", "QF",
@@ -30,7 +34,7 @@ public class ExcelConverter {
     	"SUM-", "SUM~"
     };
     
-    static String [] state2 = new String [] {
+    static String [] state2 = new String [] { //Second array that stores the states as outlined by the client.
         	"ATEX", "ATI", "C", "EMO+", "EMO-" ,
         	"EV+", "EV-", "G", "IP", "L",
         	"META", "O", "OE", "P", "QF",
@@ -80,22 +84,22 @@ public class ExcelConverter {
 		
 		for(int i = 0; i < 3; i++)
 		{
-			//Cell cell = row1.createCell(cellNum++);
-			//cell.setCellValue(param[i]);
 			
-			if(i == 0)
+			if(i == 0) //Sets the very first cell in the sheet as the parameter "Name" Column
 			{
 				Cell cell = row1.createCell(cellNum++);
 				cell.setCellValue("NAME");
 			}
 			
-			else if(i == 1)
+			else if(i == 1) //Sets the second cell in the sheet as the parameter "Game Type"
 			{
 				Cell cell = row1.createCell(cellNum++);
 				cell.setCellValue("GAME TYPE");
 			}
 			
-			else
+			else //For everything else, we'll be using the Cartesan product in order to account for all the different
+				//combinations of states.
+				
 			{
 				for(int j = 0; j < state.length; j++)
 				{
@@ -123,6 +127,54 @@ public class ExcelConverter {
 				}));
 		
 	}//end dirReader
+	
+//===================================================================
+	public List<String> getName(List<String> fileList)
+	{
+		List<String> nameList = new ArrayList<String>(); //create an array list named "nameList" Will use in order to store just the name from the file name.
+	
+		
+		for(int i = 0; i < fileList.size(); i++) //Begin for loop in order to traverse the inputed fileList
+		{
+			nameList.add(fileList.get(i).substring(0, 4)); //Adds the first four characters within the string from fileList. These four characters appear to be the "name" consistent across all .txt files
+		}//End for loop
+		
+		return nameList; //Returns nameList
+		
+	}//end getName
+//===================================================================
+	public void writeNamesAndGame(List<String> nameList, List<String>gameList)
+	{
+		int rownum = 1; //Keeps track of the row number
+		
+		for(int i = 0; i < nameList.size(); i++)
+		{
+			rowN = firstSheet.createRow(rownum); //Creates a row based off of the rownum variable
+			Cell cell0 = rowN.createCell(0); //Creates a cell that will always be at position 0 on any row
+			cell0.setCellValue(nameList.get(i)); //Inserts a name from nameList at position i
+			
+			Cell cell1 = rowN.createCell(1); //Creates a cell that will always be at position 0 on any row
+			cell1.setCellValue(gameList.get(i)); //Inserts a game type from nameList at position i
+			
+			rownum++;
+		}//end for
+		
+	}//end writeNamesAndGame
+//===================================================================
+	public List<String> getGameType(List<String> fileList)
+	{
+		List<String> gameList = new ArrayList<String>(); //creates a list that will store game types
+		
+		
+		for(int i = 0; i < fileList.size(); i++) //loops through the entire fileList
+		{
+			String[] array = fileList.get(i).split("\\."); //Splits the string by "." and places them into an array. This will continuously initialize as it loops through
+			gameList.add(array[2]); //Adds whatever is in array[2]. [2] is where the game type would be stored after splitting the String
+		}
+		
+		return gameList; 
+	}
+
 //===================================================================
 
 	public static void main(String[] args) throws IOException {
@@ -130,7 +182,8 @@ public class ExcelConverter {
 		ExcelConverter in = new ExcelConverter();
 		in.writeParams();
 		in.dirReader();
-		System.out.println(fileList);
+		in.writeNamesAndGame(in.getName(fileList), in.getGameType(fileList));
+		
 
         // To write out the workbook into a file we need to create an output
         // stream where the workbook content will be written to.
